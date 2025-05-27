544a82133ed413435c52072ff921f11edae80edb
datdang2712 <dangbin2712@gmail.com>
2025-05-27 16:42:30 +0700

    return full history

--- app/db/database-init/create-views.sql
+++ app/db/database-init/create-views.sql
@@ -13,6 +13,7 @@ JOIN Questions q ON r.QID = q.QID
 JOIN Answers a ON r.AnswerID = a.AnswerID
 GROUP BY q.QID, a.AnswerID;
 
+drop view if exists v_history_response;
 CREATE VIEW v_history_response AS
 SELECT
 r.ResponseID,
@@ -22,14 +23,19 @@ r.Employment,
 r.Country,
 r.EdLevel,
 r.UID,
-GROUP_CONCAT(CASE WHEN q.qname = 'LanguageWorkedWith' THEN a.Answer END SEPARATOR ';') AS LanguageWorkedWith,
+GROUP_CONCAT(CASE WHEN q.qname = 'LanguageHaveWorkedWith' THEN a.Answer END SEPARATOR ';') AS LanguageHaveWorkedWith,
 GROUP_CONCAT(CASE WHEN q.qname = 'LanguageWantToWorkWith' THEN a.Answer END SEPARATOR ';') AS LanguageWantToWorkWith,
-GROUP_CONCAT(CASE WHEN q.qname = 'AISelect' THEN a.Answer END SEPARATOR ';') AS AISelect,
+GROUP_CONCAT(CASE WHEN q.qname = 'DatabaseHaveWorkedWith' THEN a.Answer END SEPARATOR ';') AS DatabaseHaveWorkedWith,
+GROUP_CONCAT(CASE WHEN q.qname = 'DatabaseWantToWorkWith' THEN a.Answer END SEPARATOR ';') AS DatabaseWantToWorkWith,
+GROUP_CONCAT(CASE WHEN q.qname = 'WebframeHaveWorkedWith' THEN a.Answer END SEPARATOR ';') AS WebframeHaveWorkedWith,
+GROUP_CONCAT(CASE WHEN q.qname = 'WebframeWantToWorkWith' THEN a.Answer END SEPARATOR ';') AS WebframeWantToWorkWith,
 GROUP_CONCAT(CASE WHEN q.qname = 'LearnCode' THEN a.Answer END SEPARATOR ';') AS LearnCode,
-GROUP_CONCAT(CASE WHEN q.qname = 'LearningSources' THEN a.Answer END SEPARATOR ';') AS LearningSources,
-GROUP_CONCAT(CASE WHEN q.qname = 'Motivation' THEN a.Answer END SEPARATOR ';') AS Motivation,
-GROUP_CONCAT(CASE WHEN q.qname = 'OpSysProfessional use' THEN a.Answer END SEPARATOR ';') AS 'OpSysProfessional use'
--- Add more CASEs for other qname values if needed
+GROUP_CONCAT(CASE WHEN q.qname = 'LearnCodeOnline' THEN a.Answer END SEPARATOR ';') AS LearnCodeOnline,
+GROUP_CONCAT(CASE WHEN q.qname = 'AISelect' THEN a.Answer END SEPARATOR ';') AS AISelect,
+GROUP_CONCAT(CASE WHEN q.qname = 'AIThreat' THEN a.Answer END SEPARATOR ';') AS AIThreat,
+GROUP_CONCAT(CASE WHEN q.qname = 'AIToolCurrentlyUsing' THEN a.Answer END SEPARATOR ';') AS AIToolCurrentlyUsing,
+GROUP_CONCAT(CASE WHEN q.qname = 'OpSysPersonalUse' THEN a.Answer END SEPARATOR ';') AS OpSysPersonalUse,
+GROUP_CONCAT(CASE WHEN q.qname = 'OpSysProfessionalUse' THEN a.Answer END SEPARATOR ';') AS OpSysProfessionalUse
 FROM Respondents r
 LEFT JOIN Responses rs ON r.ResponseID = rs.ResponseID
 LEFT JOIN Answers a ON a.AnswerID = rs.AnswerID
